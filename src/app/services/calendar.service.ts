0a9f25ed6dbdd041e3534b477eb1d2fc26023b32
Marcos Carlomagno <marcoscarlomagno1@gmail.com>
2020-08-10 09:50:17 -0300

    add: load reservations

--- src/app/services/calendar.service.ts
+++ src/app/services/calendar.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { Sala } from '../models/sala.model';
 import { Observable } from 'rxjs';
@@ -10,6 +10,7 @@ import { Reservation } from '../models/reservation.model';
 })
 export class CalendarService {
 
+
   private url = environment.url;
 
   constructor(private http: HttpClient) { }
@@ -19,6 +20,15 @@ export class CalendarService {
       reservation,
     };
     return this.http.post<any>(this.url + '/reservation', body);
+  }
+
+  getRoomsReservations(currentStart: Date, currentEnd: Date, salaId?: number): Observable<any>  {
+    return this.http.get<any>(this.url + '/reservation/byWeek', {
+      params: new HttpParams()
+        .set('idSala', salaId ? salaId.toString() : '')
+        .set('dateFrom', currentStart.toISOString())
+        .set('dateTo', currentEnd.toISOString())
+    });
   }
 
 }
